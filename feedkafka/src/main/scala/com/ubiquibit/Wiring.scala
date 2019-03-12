79b303a8e7458f9c44fb379948de23b48bc1b3a2
jasonnerothin@gmail.com <jasonnerothin@gmail.com>
2019-03-12 11:33:06 -0500

    adding comment

--- feedkafka/src/main/scala/com/ubiquibit/Wiring.scala
+++ feedkafka/src/main/scala/com/ubiquibit/Wiring.scala
@@ -3,6 +3,10 @@ package com.ubiquibit
 import com.ubiquibit.buoy.jobs.{InitKafka, InitKafkaImpl, InitRedis, InitRedisImpl}
 import com.ubiquibit.buoy.{FileReckoning, FileReckoningImpl}
 
+/**
+  * As described here: http://jonasboner.com/real-world-scala-dependency-injection-di/
+  * in "other alternative #1"
+  */
 object Wiring {
 
   lazy val fileReckoning: FileReckoning = new FileReckoningImpl()
