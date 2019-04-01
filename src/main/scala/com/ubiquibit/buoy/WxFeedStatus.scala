52928a4c44ebeec53f914263f165e5fb0307afae
jasonnerothin@gmail.com <jasonnerothin@gmail.com>
2019-04-01 16:02:48 -0500

    Adding CCAL 4.0

--- src/main/scala/com/ubiquibit/buoy/WxFeedStatus.scala
+++ src/main/scala/com/ubiquibit/buoy/WxFeedStatus.scala
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2019.
+ *
+ * This work, (c) by jason@ubiquibit.com
+ *
+ * This work is licensed under a
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+ *
+ */
+
 package com.ubiquibit.buoy
 
 sealed abstract class WxFeedStatus(description: String){
